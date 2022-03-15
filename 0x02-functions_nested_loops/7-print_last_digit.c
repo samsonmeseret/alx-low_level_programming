5c712411e8ca6b78f0cf80d034ae7eff5eafa109
Samson Meseret <80979055+samsonmeseret@users.noreply.github.com>
2022-03-15 15:22:36 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,16 @@
 
 int print_last_digit(int n)
 {
-	n = n % 10;
+	int last_digit;
 
-	if (n < 0)
-		n = -n;
-		_putchar(n + '0');
-		return (n);
+	last_digit = n % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+	}
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
