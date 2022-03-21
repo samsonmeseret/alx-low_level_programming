bca8bf59208431c43f2c6f5af98ec6edfcb56a90
Samson Meseret <80979055+samsonmeseret@users.noreply.github.com>
2022-03-21 11:55:50 -0700

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,19 +6,18 @@
  *
  */
 
+
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i;
 
 	while (str[i] != '\0')
 	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 		i++;
-	}
-
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
 	}
 	_putchar('\n');
 }
